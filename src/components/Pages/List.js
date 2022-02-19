a564fb3a746748ac45caddf2e21e118a0a367e75
Tavie Kittredge <taviekit@gmail.com>
2022-02-19 16:05:42 -0500

    Create table for list

--- src/components/Pages/List.js
+++ src/components/Pages/List.js
@@ -4,6 +4,15 @@ export const List = () => {
   return (
     <div>
       <h1>List</h1>
+      <table>
+        <tr>
+          <th>Lottery Name</th>
+          <th>Track Progress</th>
+          <th>Email</th>
+          <th>Join</th>
+        </tr>
+        //dynamically map inside info
+      </table>
     </div>
   );
 };
